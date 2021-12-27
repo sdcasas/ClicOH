ff468729c1fe3ab9fad79614147e4dba8708fa78
Sergio Casas <sdcasas@gmail.com>
2021-12-27 19:24:10 -0300

    ADD test to product

--- src/apps/core/tests/test_product.py
+++ src/apps/core/tests/test_product.py
@@ -1,23 +1,15 @@
 import json
-
 import pytest
 
-from rest_framework.test import APIClient
-
 from core.models import Product
+from .conftest import get_client
 
-
-def get_client():
-    client = APIClient()
-    client.login(username='admin', password='asdf1234')
-    return client
 
 @pytest.mark.django_db()
 def test_product_create(user_admin_create):
 
     URL = "/api/product/"
     product_name = "Notebook Asus"
-
     data = {
         "name": product_name,
         "price": 350000,
@@ -53,22 +45,67 @@ def test_product_list(user_admin_create, product_create_multiple):
 
 
 @pytest.mark.django_db()
+def test_product_detail(user_admin_create, product_create_multiple):
+
+    product = Product.objects.get(name='Notebook HP')
+
+    assert Product.objects.count() == 3
+
+    URL = f"/api/product/{product.id}/"
+    client = get_client()
+    response = client.get(URL)
+
+    data = response.json()["data"]["attributes"]
+    assert response.status_code == 200
+    assert response.json()["data"]["id"] == str(product.id)
+    assert data["name"] == product.name
+    assert data["price"] == product.price
+    assert data["stock"] == product.stock
+
+
+@pytest.mark.django_db()
 def test_product_update(user_admin_create, product_create_multiple):
 
     product = Product.objects.get(name='Notebook HP')
     URL = f"/api/product/{product.id}/"
-    data_new = {
-        "data": {
-            "type": "Product",
-            "id": product.id,
-            "attributes": {
-                "name": 'Notebook HP Pavilon'
-            }
-        }
-    }
+    data_new = {"name": "Notebook HP Pavilon"}
     client = get_client()
-    response = client.put(URL, data=json.dumps(data_new))
+    response = client.patch(URL, data=data_new)
 
     product.refresh_from_db()
-    assert response.status_code == 301
+    assert response.status_code == 200
+    assert response.status_text == "OK"
     assert product.name == "Notebook HP Pavilon"
+
+
+@pytest.mark.django_db()
+def test_product_stock_update(user_admin_create, product_create_multiple):
+
+    product = Product.objects.get(name='Notebook HP')
+
+    assert product.stock == 7
+
+    URL = f"/api/product/{product.id}/"
+    data_new = {"stock": 20}
+    client = get_client()
+    response = client.patch(URL, data=data_new)
+
+    product.refresh_from_db()
+    assert response.status_code == 200
+    assert product.stock == 20
+
+
+@pytest.mark.django_db()
+def test_product_delete(user_admin_create, product_create_multiple):
+
+    product = Product.objects.get(name='Notebook HP')
+
+    assert Product.objects.count() == 3
+
+    URL = f"/api/product/{product.id}/"
+    client = get_client()
+    response = client.delete(URL)
+
+    assert response.status_code == 204
+    assert Product.objects.count() == 2
+    assert Product.objects.filter(id=product.id).exists() == False
