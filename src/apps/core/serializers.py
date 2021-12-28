b0227fd68671260a14934331c17fd6c7abdf738b
Sergio Casas <sdcasas@gmail.com>
2021-12-28 12:25:04 -0300

    ADD perform_create

--- src/apps/core/serializers.py
+++ src/apps/core/serializers.py
@@ -14,15 +14,29 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    get_total = serializers.SerializerMethodField(method_name='get_total')
-    get_total_usd = serializers.SerializerMethodField(method_name='get_total_usd')
+    details = serializers.StringRelatedField(many=True)
+    #details = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Order
-        fields = ('datetime_register', 'get_total', 'get_total_usd')
+        fields = ('datetime_register', "details",  'get_total', 'get_total_usd')
+
+    get_total = serializers.SerializerMethodField(method_name='get_total')
+    get_total_usd = serializers.SerializerMethodField(method_name='get_total_usd')
+
+    """included_serializers = {
+        'orderdetail': 'core.serializers.OrderDetailSerializer',
+    }"""
 
     def get_total(self, order):
         return order.get_total()
 
     def get_total_usd(self, order):
         return order.get_total_usd()
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderDetail
+        fields = ('order', 'product', 'cuantity')
--- src/config/router.py
+++ src/config/router.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from core.views import ProductViewSet, OrderViewSet
+from core.views import ProductViewSet, OrderViewSet, OrderDetailViewSet
 
 
 router = DefaultRouter()
 
 router.register('product', ProductViewSet, basename='product')
 router.register('order', OrderViewSet, basename='order')
+router.register('orderdetail', OrderDetailViewSet, basename='orderdetail')
